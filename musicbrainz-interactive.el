ef876aa8cf45745a34de86a4ec9b00b9cb5e14c1
deadendpl <oliwier.czerwi@proton.me>
2026-07-21 18:24:13 +0200

    fixed the cdstub in musicbrainz-interactive

--- musicbrainz-interactive.el
+++ musicbrainz-interactive.el
@@ -4,7 +4,7 @@
 
 ;; Author:  Oliwier Czerwiński <oliwier.czerwi@proton.me>
 ;; Keywords: data, convenience, music
-;; Version: 20260712
+;; Version: 20260721
 ;; Package-Requires: ((emacs "28.1") (musicbrainz "0.1"))
 ;; URL: https://github.com/zzkt/metabrainz
 
@@ -522,9 +522,9 @@ descriptions."
   "Open the CDSTUB in a MusicBrainz webpage."
   (interactive
    (list
-    (read-string "CD stub: " nil 'musicbrainz-interactive-CD
-                 stub-query-history)))
-  (browse-url (format "https://musicbrainz.org/cdstub/%s" cdstub)))
+    (read-string "CD stub: " nil
+                 'musicbrainz-interactive-cdstub-query-history)))
+  (browse-url (Format "https://musicbrainz.org/cdstub/%s" cdstub)))
 
 ;;;###autoload
 (defun musicbrainz-interactive-search-cdstub
